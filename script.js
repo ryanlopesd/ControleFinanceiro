537c06392edacb78277aea597631a1d389d5f788
Ryan Lopes de Aquino <rlaquino2002@gmail.com>
2026-03-10 21:36:27 -0300

    feat();adiciona salvamento local storage

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-console.log("Olá, mundo!");
-
-var arrayTabela = [
-    { id: 1, descricao: "Conta de Luz", valor: 100.0, tipo: "saida" },
-    { id: 2, descricao: "Salário", valor: 2500.0, tipo: "entrada" },
-    { id: 3, descricao: "Supermercado", valor: 300.0, tipo: "saida" },
-];
+var arrayTabela = [];
 
 function salvarLocalStorage() {
     localStorage.setItem("arrayTabela", JSON.stringify(arrayTabela));
 }
 
 function carregarTabela() {
+    arrayTabela = JSON.parse(localStorage.getItem("arrayTabela"));
     const tabela = document.getElementById("tabela");
     tabela.innerHTML = "";
 
@@ -18,8 +13,8 @@ function carregarTabela() {
         const tr = document.createElement("tr");
     tr.innerHTML = `
         <td>${item.descricao}</td>
-        <td>R$ ${item.valor.toFixed(2)}</td>
-        <td>${item.tipo === "entrada" ? "Entrada" : "Saída"}</td>
+        <td>R$ ${item.valor}</td>
+        <td>${item.tipo}</td>
         <td style="text-align: center;"><i class="fa-solid fa-trash" onclick="removerItem(${arrayTabela.indexOf(item)})"></i></td>
     `;
     tabela.appendChild(tr);})
@@ -27,15 +22,20 @@ function carregarTabela() {
 
 function adicionarItem() {
     const descricaoInput = document.getElementById("descricao").value.trim();
-    const valorInput = document.getElementById("valor").value.trim();
-    //const tipoSelect = document.getElementById("tipo").value.trim();
-    console.log(descricaoInput, valorInput);
-    if (!descricao || !valor) {
+    const valorInput = parseFloat(document.getElementById("valor").value.trim());
+    const tipoRadio = document.querySelector('input[name="tipo"]:checked');
+    let tipoInput;
+    
+    if(tipoRadio) {
+        tipoInput = tipoRadio.value;
+    }
+
+    if (!descricaoInput || !valorInput || !tipoRadio) {
         alert("Preencha todos os campos!");
         return;
     }
     const novoId = arrayTabela.length > 0 ? arrayTabela[arrayTabela.length - 1].id + 1 : 1;
-    arrayTabela.push({ id: novoId, descricao, valor });
+    arrayTabela.push({ id: novoId, descricao: descricaoInput, valor: valorInput, tipo: tipoInput });
 
     salvarLocalStorage();
     carregarTabela();
@@ -43,4 +43,6 @@ function adicionarItem() {
     // limpa os inputs
     document.getElementById("valor").value = "";
     document.getElementById("descricao").value = "";
+    document.getElementById("entrada").checked = false;
+    document.getElementById("saida").checked = false;
 }
